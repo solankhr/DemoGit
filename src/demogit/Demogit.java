2e3198fcf9cecc3a7002bed3fd635943757a84fd
hrith <hrith@10.48.49.42>
2023-05-25 10:10:51 -0400

    abcdefgh

--- src/demogit/Demogit.java
+++ src/demogit/Demogit.java
@@ -16,6 +16,7 @@ public class Demogit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("World");
     }
     
 }
